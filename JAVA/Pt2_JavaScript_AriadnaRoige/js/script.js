0ecc63ad9c84f57f08748ea5d246d68b50b17484
a3d2_AriadnaRoige <ariadna.roige.7e6@itb.cat>
2024-03-05 17:47:19 +0100

    javsas

--- JAVA/Pt2_JavaScript_AriadnaRoige/js/script.js
+++ JAVA/Pt2_JavaScript_AriadnaRoige/js/script.js
@@ -2,3 +2,41 @@ var contenido = document.getElementById("contenido");
 contenido.innerHTML = "Mi Nomnbre Es Ari";
 
 console.log("Probando la Consola: Hola");
+
+//VARIABLES
+var mensaje = "Hola" //String o cadenas
+console.log(mensaje);
+
+var num1 = 8; //datos numericos
+var num2 = 10;
+console.log(num1+num2);
+
+var activo = true; //datos booleanos
+
+var saludo = mensaje; //a una variable se le asigna el valor de otra
+console.log(saludo);
+
+//CONDICIONES
+function saludar2(){
+    var saludo = "Hola, esto es JS";
+    var antiSaludo = "No me digas nada, odio JS";
+    var contento = true;
+
+    if (contento === true){
+        console.log(saludo);
+    }else{
+        console.log(antiSaludo);
+    }
+
+}
+
+//LOOPS - BUCLES
+
+var a3d2 = ["Sebas", "Fu", "Aina", "Laura", "Oscar", "Gerard", "Javi", "Marta", "Jordi", "Adri"]
+function saludar3(nombre){
+    console.log("Hola, "+nombre)
+}
+
+a3d2.forEach(function(elem){
+    saludar3(elem);
+})
