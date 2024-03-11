ccad18f985dbae4e079d7666234fcde390668105
a3d2_AriadnaRoige <ariadna.roige.7e6@itb.cat>
2024-03-11 18:30:17 +0100

    PizzA

---
+++ JAVA/topping-pizzas/js/script.js
@@ -0,0 +1,11 @@
+/*
+> Seleccionar Elementos con JavaScript.
+*/
+
+// Seleccionar elemento por id.
+
+// Contenedor
+const contenedor = document.getElementById('contenedor');
+console.log(contenedor);
+console.log(contenedor.innerHTML);
+console.log(typeof contenedor.innerHTML);
