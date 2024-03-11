21532e34bf86bff4069f5c025a457cbc4a6b1ec6
a3d2_AriadnaRoige <ariadna.roige.7e6@itb.cat>
2024-03-11 19:05:57 +0100

    java video 1.33

--- JAVA/topping-pizzas/js/script.js
+++ JAVA/topping-pizzas/js/script.js
@@ -9,3 +9,51 @@ const contenedor = document.getElementById('contenedor');
 console.log(contenedor);
 console.log(contenedor.innerHTML);
 console.log(typeof contenedor.innerHTML);
+
+
+const titulo = document.getElementById('titulo');
+console.log(titulo);
+console.log(typeof titulo);
+console.log(titulo.innerText);
+console.log(typeof titulo.innerText);
+console.log(titulo.tagName);
+
+// Seleccionar elemento por clase.
+const toppings = document.getElementsByClassName('toppings');
+console.log(toppings);
+console.log(toppings.length);
+console.log(toppings[0]);
+console.log(toppings[0].id);
+console.log(toppings[1 /* 0,1,2,3 o 4 en este caso */ ]);
+
+// Seleccionar elemento por etiquetaHTML.
+const misToppings = document.getElementsByTagName('li');
+console.log(misToppings);
+
+const toppingsFondoMarron = document.getElementsByTagName('fondo-marron');
+console.log(toppingsFondoMarron);
+
+
+// Seleccionar elemento por SelectorCSS (Normal y All).
+
+/* const aceitunas = document.querySelector('#aceitunas');
+console.log(aceitunas); */
+const aceitunas = document.querySelector('.topping' /* Primer elemenot que cumpla los requisitos */);
+console.log(aceitunas);
+console.log(typeof aceitunas);
+
+const primerToppingNaranja = document.querySelector('.topping.fondo-naranja');
+console.log(primerToppingNaranja);
+console.log(typeof primerToppingNaranja);
+/* const primerToppingNaranja = document.querySelector('ul li.fondo-naranja');
+console.log(primerToppingNaranja); */
+/* const primerToppingNoMarron = document.querySelector('ul li:not(.fondo-marron)');
+console.log(primerToppingNoMarron); */
+___
+
+const toppingsFondoNaranja = document.querySelectorAll('.topping.fondo-naranja');
+console.log(toppingsFondoNaranja);
+console.log(toppingsFondoNaranja[0]);
+console.log(toppingsFondoNaranja[1]);
+console.log(toppingsFondoNaranja.length);
+console.log(typeof toppingsFondoNaranja);
