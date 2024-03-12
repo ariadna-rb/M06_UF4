0a9de5e9a6d5f1d650b9d04f43e8c89ca99cd9a3
a3d2_AriadnaRoige <ariadna.roige.7e6@itb.cat>
2024-03-12 18:49:38 +0100

    hbn

--- JAVA/DOM-ProyectoGrupos/js/app.js
+++ JAVA/DOM-ProyectoGrupos/js/app.js
@@ -4,39 +4,40 @@ const listaDeTarea = document.getElementById('lista-de-tareas');
 
 function agregarTares(){
     if (input.value){
-        //Creación de tarea
-         let tareaNueva = document.createElement('div');
-         tareaNueva.classList.add('tarea');
+     //Creación de tarea
+     let tareaNueva = document.createElement('div');
+     tareaNueva.classList.add('tarea');
 
-         //Texto que escribe el usuario
-         let texto = document.createElement('p');
-         texto.innerText = input.value;
-         tareaNueva.appendChild(texto);
+     //Texto que escribe el usuario
+     let texto = document.createElement('p');
+     texto.innerText = input.value;
+     tareaNueva.appendChild(texto);
 
-         //Crear y agregar contenedor de iconos
-         let iconos = document.createElement('div');
-         iconos.classList.add('iconos');
-         tareaNueva.appendChild(iconos);
+     //Crear y agregar contenedor de iconos
+     let iconos = document.createElement('div');
+     iconos.classList.add('iconos');
+     tareaNueva.appendChild(iconos);
 
-         //Iconos
-         let completar = document.createElement('i');
-         completar.classList.add('bi', 'bi-check-circle-fill', 'icono-completar');
-         completar.addEventListener('click', completarTarea);
+     //Iconos
+     let completar = document.createElement('i');
+     completar.classList.add('bi', 'bi-check-circle-fill', 'icono-completar');
+     completar.addEventListener('click', completarTarea);
        
-         let eliminar = document.createElement('i');
-         eliminar.classList.add('bi', 'bi-trash3-fill', 'icono-eliminar');
-         eliminar.addEventListener('click', eliminarTarea);
+     let eliminar = document.createElement('i');
+     eliminar.classList.add('bi', 'bi-trash3-fill', 'icono-eliminar');
+     eliminar.addEventListener('click', eliminarTarea);
        
-         iconos.append(completar, eliminar);
+     iconos.append(completar, eliminar);
 
-        // Agregar la tarea a la lista.
-        listaDeTareas.appendChild(tareaNueva);
+     // Agregar la tarea a la lista.
+     listaDeTareas.appendChild(tareaNueva);
         
-        } else {
-        alert('Por favor ingresa una tarea.');
-        }
-
+    } else {
+     alert('Por favor ingresa una tarea.');
     }
+}
+
+boton.addEventListener('click','agregarTarea');
 
 // Marcar una tarea como completada.
 function completarTarea(e) {
